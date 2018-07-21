38f35d44aba533394a1d7ac2bb88ef052bfd0816
sueka <mail@sueka.me>
2018-07-22 03:05:26 +0900

    :rotating_light: reduce operations dependent on $0

--- bin/sign.sh
+++ bin/sign.sh
@@ -264,7 +264,7 @@ sign_migrate() {
 
 	# $SIGN_CONFIG_DIR が存在しない場合
 	if ! [ -d "$SIGN_CONFIG_DIR" ]; then
-		echo_fatal "$NAME is not initialized." >&2
+		echo_fatal 'Execute `sign init`.' >&2
 		exit $EX_IOERR
 	fi
 

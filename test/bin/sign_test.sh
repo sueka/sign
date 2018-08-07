90f3f62799944310752af4075686face8b09f251
sueka <mail@sueka.me>
2018-08-07 23:57:06 +0900

    :rotating_light: update the assert helper

--- test/bin/sign_test.sh
+++ test/bin/sign_test.sh
@@ -155,7 +155,10 @@ assert() {
 
 	set_current=$(set +o)
 	set +e
-	eval "$command" 1>"$PROJECT_ROOT_DIR/test/tmp/dev/stdout" 2>"$PROJECT_ROOT_DIR/test/tmp/dev/stderr"
+
+	# NOTE: exit する関数を呼ぶと errexit off でも終了してしまうが、パイプラインの一部として呼ぶと回避できる。
+	: | eval "$command" 1>"$PROJECT_ROOT_DIR/test/tmp/dev/stdout" 2>"$PROJECT_ROOT_DIR/test/tmp/dev/stderr"
+
 	actual_exit_status=$?
 	eval "$set_current"
 

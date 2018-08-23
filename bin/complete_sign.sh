6073299825b1559738ec4944f12a2060ce1216bc
sueka <mail@sueka.me>
2018-08-23 22:00:41 +0900

    :art: format & make exit code proper

--- bin/complete_sign.sh
+++ bin/complete_sign.sh
@@ -3,6 +3,8 @@
 EX_OK=0
 EX_USAGE=64
 
+SIGN_CONFIG_DIR="$HOME/.sign"
+
 _complete_sign() {
 	COMPREPLY=()
 
@@ -28,7 +30,7 @@ _complete_sign() {
 	if (( $COMP_CWORD == 2 )); then
 		case "$subcommand" in
 			'register' | 'get' )
-				service_names=$(cat ~/.sign/service_names)
+				service_names=$(cat "$SIGN_CONFIG_DIR/service_names")
 				COMPREPLY=($(compgen -W "$service_names" -- "$cur"))
 
 				return $EX_OK
@@ -36,7 +38,7 @@ _complete_sign() {
 
 			'init' | 'migrate' )
 				if [ -n "$cur" ]; then
-					return $EX_USAGE
+					return $EX_OK
 				fi
 
 				return $EX_OK
@@ -47,14 +49,14 @@ _complete_sign() {
 	if (( $COMP_CWORD == 3 )); then
 		case "$subcommand" in
 			'get' )
-				service_names=$(cat ~/.sign/service_names)
+				service_names=$(cat "$SIGN_CONFIG_DIR/service_names")
 				service_name=$operand_1
 
 				if ! echo "$service_names" | grep "^$service_name\$" 1>/dev/null; then
-					return $EX_USAGE
+					return $EX_OK
 				fi
 
-				your_ids=$(cat ~/".sign/${service_name}_ids")
+				your_ids=$(cat "$SIGN_CONFIG_DIR/${service_name}_ids")
 				COMPREPLY=($(compgen -W "$your_ids" -- "$cur"))
 
 				return $EX_OK
@@ -62,7 +64,7 @@ _complete_sign() {
 
 			'init' | 'register' | 'migrate' )
 				if [ -n "$cur" ]; then
-					return $EX_USAGE
+					return $EX_OK
 				fi
 
 				return $EX_OK

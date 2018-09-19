a3b0de582d00ee3f72c64f8c27b264a88dfbd0d6
sueka <mail@sueka.me>
2018-09-20 04:23:26 +0900

    :hammer: update the interface of assert

--- test/bin/sign_test.sh
+++ test/bin/sign_test.sh
@@ -89,7 +89,7 @@ main_test() {
 		return $EX_USAGE
 	fi
 
-	assert 'main' $EX_USAGE
+	assert -x $EX_USAGE 'main'
 
 	PATH="$PATH_IGNORING_STTY" sign_init_test 'main init'
 	PATH="$PATH_IGNORING_STTY" sign_register_test 'main register' passphrase
@@ -125,41 +125,41 @@ sign_init_test() {
 	sign_init_command=$1 && shift
 
 	setup_for_sign_init
-	assert "echo 'passphrase${LF}passphrase' | $sign_init_command" $EX_OK
+	assert -x $EX_OK "echo 'passphrase${LF}passphrase' | $sign_init_command"
 
 	setup_for_sign_init
-	assert "echo '#${LF}#' | $sign_init_command" $EX_OK
+	assert -x $EX_OK "echo '#${LF}#' | $sign_init_command"
 
 	setup_for_sign_init
-	assert "echo 'elif${LF}elif' | $sign_init_command" $EX_OK
+	assert -x $EX_OK "echo 'elif${LF}elif' | $sign_init_command"
 
 	setup_for_sign_init
-	assert "echo '.${LF}.' | $sign_init_command" $EX_OK
+	assert -x $EX_OK "echo '.${LF}.' | $sign_init_command"
 
 	setup_for_sign_init
-	assert "echo 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.${LF}Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.' | $sign_init_command" $EX_OK
+	assert -x $EX_OK "echo 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.${LF}Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.' | $sign_init_command"
 
 	setup_for_sign_init
 	# passphrase と passphrase_again は完全に一致しなければならない。
-	assert "echo 'passphrase${LF}passphrase ' | $sign_init_command" $EX_SOFTWARE
+	assert -x $EX_SOFTWARE "echo 'passphrase${LF}passphrase ' | $sign_init_command"
 
 	setup_for_sign_init
 	# sign_init はオプションを受け付けない。
-	assert "$sign_init_command :" $EX_USAGE
+	assert -x $EX_USAGE "$sign_init_command :"
 
 	setup_for_sign_init
 	# passphrase は空文字列であってもよい。
-	assert "echo '' | $sign_init_command" $EX_OK
+	assert -x $EX_OK "echo '' | $sign_init_command"
 
 	setup_for_sign_init
 	# passphrase は空白のみでもよい。
-	assert "echo ' $LF ' | $sign_init_command" $EX_OK
+	assert -x $EX_OK "echo ' $LF ' | $sign_init_command"
 
 	setup_for_sign_init
 	# 空白の種類は区別される。
-	assert "echo ' $LF	' | $sign_init_command" $EX_SOFTWARE
-	assert "echo '	$LF　' | $sign_init_command" $EX_SOFTWARE
-	assert "echo '　$LF ' | $sign_init_command" $EX_SOFTWARE
+	assert -x $EX_SOFTWARE "echo ' $LF	' | $sign_init_command"
+	assert -x $EX_SOFTWARE "echo '	$LF　' | $sign_init_command"
+	assert -x $EX_SOFTWARE "echo '　$LF ' | $sign_init_command"
 }
 
 #
@@ -188,15 +188,15 @@ sign_register_test() {
 	passphrase=$1 && shift
 
 	setup_for_sign_register "$passphrase"
-	assert "echo '$passphrase${LF}' | $sign_register_command GitHub sueka" $EX_OK
-	assert "echo '$passphrase${LF}hsueka${LF}' | $sign_register_command Twitter" $EX_OK
-	assert "echo '$passphrase${LF}Stack Overflow${LF}8795737${LF}' | $sign_register_command" $EX_OK
+	assert -x $EX_OK "echo '$passphrase${LF}' | $sign_register_command GitHub sueka"
+	assert -x $EX_OK "echo '$passphrase${LF}hsueka${LF}' | $sign_register_command Twitter"
+	assert -x $EX_OK "echo '$passphrase${LF}Stack Overflow${LF}8795737${LF}' | $sign_register_command"
 
 	# すでにパスワードが発行されたことがあるサービスのパスワード長は尋ねられない。
-	assert "echo '$passphrase${LF}pipibaoni' | $sign_register_command Twitter" $EX_OK
+	assert -x $EX_OK "echo '$passphrase${LF}pipibaoni' | $sign_register_command Twitter"
 
 	# すでにパスワードが発行されてゐる ID のパスワードは発行できない。
-	assert "echo '$passphrase${LF}GitHub${LF}sueka' | $sign_register_command" $EX_SOFTWARE
+	assert -x $EX_SOFTWARE "echo '$passphrase${LF}GitHub${LF}sueka' | $sign_register_command"
 }
 
 #
@@ -207,10 +207,10 @@ hexadecimal_to_duohexagesimal_test() {
 		return $EX_USAGE
 	fi
 
-	assert 'hexadecimal_to_duohexagesimal 0' 0 '0'
-	assert 'hexadecimal_to_duohexagesimal F' 0 'F'
-	assert 'hexadecimal_to_duohexagesimal FF' 0 '47'
-	assert 'hexadecimal_to_duohexagesimal FFFFFFFFFFFF' 0 '1HvWXNAa7'
+	assert -x $EX_OK -o'0' 'hexadecimal_to_duohexagesimal 0'
+	assert -x $EX_OK -o'F' 'hexadecimal_to_duohexagesimal F'
+	assert -x $EX_OK -o'47' 'hexadecimal_to_duohexagesimal FF'
+	assert -x $EX_OK -o'1HvWXNAa7' 'hexadecimal_to_duohexagesimal FFFFFFFFFFFF'
 }
 
 #
@@ -221,8 +221,8 @@ hmac_sha256_test() {
 		return $EX_USAGE
 	fi
 
-	assert 'hmac_sha256 " " " "' 0 '1352c6b2598324a5fb3ad64097ca2d678ddb71d906aa994e2fd0678e0be361aa'
-	assert 'hmac_sha256 message secret' 0 '8b5f48702995c1598c573db1e21866a9b825d4a794d169d7060a03605796360b'
+	assert -x $EX_OK -o'1352c6b2598324a5fb3ad64097ca2d678ddb71d906aa994e2fd0678e0be361aa' 'hmac_sha256 " " " "'
+	assert -x $EX_OK -o'8b5f48702995c1598c573db1e21866a9b825d4a794d169d7060a03605796360b' 'hmac_sha256 message secret'
 }
 
 #
@@ -233,31 +233,41 @@ bc_with_no_linefeeds_test() {
 		return $EX_USAGE
 	fi
 
-	assert 'echo "ibase=16; FFFFFFFFFFFF" | bc_with_no_linefeeds' 0 '281474976710655'
-	assert 'echo "obase=62; 281474976710655" | bc_with_no_linefeeds' 0 ' 01 17 57 32 33 23 10 36 07'
-	assert 'echo "obase=94; 281474976710655" | bc_with_no_linefeeds' 0 ' 04 32 01 09 77 41 88 03'
-	assert 'echo "obase=1000; 281474976710655" | bc_with_no_linefeeds' 0 ' 281 474 976 710 655'
-	assert 'echo "obase=62; 20988936657440586486151264256610222593863921" | bc_with_no_linefeeds' 0 ' 02 01 01 17 11 32 48 01 31 17 36 40 32 55 58 20 38 51 46 25 22 47 58 50 37'
+	assert -x $EX_OK -o'281474976710655' 'echo "ibase=16; FFFFFFFFFFFF" | bc_with_no_linefeeds'
+	assert -x $EX_OK -o' 01 17 57 32 33 23 10 36 07' 'echo "obase=62; 281474976710655" | bc_with_no_linefeeds'
+	assert -x $EX_OK -o' 04 32 01 09 77 41 88 03' 'echo "obase=94; 281474976710655" | bc_with_no_linefeeds'
+	assert -x $EX_OK -o' 281 474 976 710 655' 'echo "obase=1000; 281474976710655" | bc_with_no_linefeeds'
+	assert -x $EX_OK -o' 02 01 01 17 11 32 48 01 31 17 36 40 32 55 58 20 38 51 46 25 22 47 58 50 37' 'echo "obase=62; 20988936657440586486151264256610222593863921" | bc_with_no_linefeeds'
 }
 
 #
-# assert <command> <expected_exit_status> [<expected_stdout> [<expected_stderr>]]
+# assert [-x <expected_exit_status>] [-o <expected_stdout>] [-e <expected_stderr>] <command>
 #
 assert() {
-	if ! ([ 2 -le $# ] && [ $# -le 4 ]); then
+	while getopts x:o:e: OPT
+	do
+		case "$OPT" in
+			x )
+				expected_exit_status=$OPTARG
+			;;
+
+			o )
+				expected_stdout=$OPTARG
+			;;
+
+			e )
+				expected_stderr=$OPTARG
+			;;
+		esac
+	done
+
+	shift $((OPTIND - 1))
+
+	if ! [ $# -eq 1 ]; then
 		return $EX_USAGE
 	fi
 
 	command=$1 && shift
-	expected_exit_status=$1 && shift
-
-	if [ -n "$*" ]; then
-		expected_stdout=$1 && shift
-	fi
-
-	if [ -n "$*" ]; then
-		expected_stderr=$1 && shift
-	fi
 
 	# TODO: $command が実行可能であることを確認する
 

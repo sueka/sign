2e31b4cd6427332ddd5a81c24a60787da721ce58
sueka <mail@sueka.me>
2018-09-29 00:51:10 +0900

    :bug: fix a bug service name or ID leading with the - is not available

--- bin/sign.sh
+++ bin/sign.sh
@@ -228,7 +228,7 @@ sign_up() {
 	fi
 
 	# 指定されたサービス名がサービス名一覧に存在しない場合、作成する
-	if ! cut -f1 "$SIGN_CONFIG_DIR/services" | grep -q "^$service_name\$"; then
+	if ! cut -f1 "$SIGN_CONFIG_DIR/services" | grep -q -- "^$service_name\$"; then
 
 		# TODO: 似たサービス名を表示させる
 
@@ -257,7 +257,7 @@ sign_up() {
 	fi
 
 	# ID がすでに存在する場合
-	if grep -q "^$your_id\$" "$SIGN_CONFIG_DIR/${service_name}_ids"; then
+	if grep -q -- "^$your_id\$" "$SIGN_CONFIG_DIR/${service_name}_ids"; then
 		echo_fatal "$service_name ID '$your_id' does already exist." >&2
 		return $EX_SOFTWARE
 	fi
@@ -322,7 +322,7 @@ sign_in() {
 	given_service_name=$service_name
 
 	# 指定されたサービス名がサービス一覧に存在しない場合
-	while ! cut -f1 "$SIGN_CONFIG_DIR/services" | grep -q "^$service_name\$"
+	while ! cut -f1 "$SIGN_CONFIG_DIR/services" | grep -q -- "^$service_name\$"
 	do
 		if command -v peco 1>/dev/null; then
 			service_name=$(
@@ -354,7 +354,7 @@ sign_in() {
 	your_given_id=$your_id
 
 	# 指定された ID が存在しない場合
-	while ! grep -q "^$your_id\$" "$SIGN_CONFIG_DIR/${service_name}_ids"
+	while ! grep -q -- "^$your_id\$" "$SIGN_CONFIG_DIR/${service_name}_ids"
 	do
 		if command -v peco 1>/dev/null; then
 			your_id=$(
@@ -512,7 +512,7 @@ copy_password() {
 	your_id=$1 && shift
 	passphrase=$1 && shift
 
-	password_length="$(grep "^$service_name	" "$SIGN_CONFIG_DIR/services" | cut -f2)"
+	password_length="$(grep -- "^$service_name	" "$SIGN_CONFIG_DIR/services" | cut -f2)"
 
 	password=$(
 		hexadecimal_to_duohexagesimal "$(hmac_sha256 "$service_name$LF$your_id" "$passphrase")" |

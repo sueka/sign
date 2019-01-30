51a5a65def5d90e703bdcdc530caded60ea6fd18
sueka <mail@sueka.me>
2019-01-30 22:30:57 +0900

    :white_check_mark: Pass the tests

--- test/bin/sign_test.sh
+++ test/bin/sign_test.sh
@@ -136,7 +136,7 @@ sign_init_test() {
 	PATH="$PATH_IGNORING_STTY" assert -x $EX_OK "echo 'passphrase${LF}passphrase' | $sign_init_command"
 
 	# すでに初期化されている場合、初期化できない。
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "$sign_init_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "$sign_init_command"
 
 	setup_for_sign_init
 	PATH="$PATH_IGNORING_STTY" assert -x $EX_OK "echo '#${LF}#' | $sign_init_command"
@@ -213,21 +213,21 @@ sign_up_test() {
 	PATH="$PATH_IGNORING_STTY" assert -x $EX_OK -b'J3kUxlp26HhPdLIQABxnQyAv45P5f4DvoRCtRtoH0yy' "echo '$passphrase${LF}pipibaoni' | $sign_up_command Twitter"
 
 	# すでにパスワードが発行されてゐる ID のパスワードは発行できない。
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF}GitHub${LF}sueka' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF}GitHub${LF}sueka' | $sign_up_command"
 
 	# サービス名と ID は空文字列であってはならない。
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF}${LF}sueka' | $sign_up_command"
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF}MDN${LF}' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF}${LF}sueka' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF}MDN${LF}' | $sign_up_command"
 
 	# サービス名と ID は先頭または末尾にスペースを含んでいてはならない
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF} GitHub${LF}sueka' | $sign_up_command"
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF}GitHub ${LF}sueka' | $sign_up_command"
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF} GitHub ${LF}sueka' | $sign_up_command"
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF} ${LF}sueka' | $sign_up_command"
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF}MDN${LF} sueka' | $sign_up_command"
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF}MDN${LF}sueka ' | $sign_up_command"
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF}MDN${LF} sueka ' | $sign_up_command"
-	PATH="$PATH_IGNORING_STTY" assert -x $EX_SOFTWARE "echo '$passphrase${LF}MDN${LF} ' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF} GitHub${LF}sueka' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF}GitHub ${LF}sueka' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF} GitHub ${LF}sueka' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF} ${LF}sueka' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF}MDN${LF} sueka' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF}MDN${LF}sueka ' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF}MDN${LF} sueka ' | $sign_up_command"
+	PATH="$PATH_IGNORING_STTY" assert -x $EX_USAGE "echo '$passphrase${LF}MDN${LF} ' | $sign_up_command"
 }
 
 #
